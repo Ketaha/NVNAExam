67d559ba860857d50df20f52b23a9e5a3d1c141e
Kadir Kadir <dragonrevoke@gmail.com>
2022-06-08 20:01:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var array = new Stack<int>();
+
+for (int i = 0; i < Convert.ToInt32(Console.ReadLine()); i++) array.Push(i + 1);
+var arraySumSquared = array.Sum() * array.Sum();
+
+var squareSum = 0;
+while(array.Any()) squareSum += array.Peek() * array.Pop();
+
+Console.WriteLine(arraySumSquared - squareSum);
